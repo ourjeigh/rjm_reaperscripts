54d8913074c1e6575fcd5cfb4fb9fcd4cd34a01f
RJ Mattingly <RJM@suckerpunch.com>
2020-12-15 12:18:34 -0800

    String cleanup options for Preset scripts

--- SetMediaItemNamesFromPluginPreset.lua
+++ SetMediaItemNamesFromPluginPreset.lua
@@ -1,6 +1,8 @@
 -- SETTINGS
-separator = "-"
+separator = "_"
 includePluginName = true
+removeSpaces = true
+removeCompany = true
 
 -- MAIN
 for i=0, reaper.CountSelectedMediaItems(0)-1 do
@@ -10,8 +12,10 @@ for i=0, reaper.CountSelectedMediaItems(0)-1 do
 	fullName = ""
 	if includePluginName then
 		success, pluginName = reaper.TrackFX_GetFXName(track, 0, pluginName)
-		pluginName = string.gsub(pluginName, "VST: ","")
-		pluginName = string.gsub(pluginName, "VST3: ","")
+		pluginName = string.gsub(pluginName, "VST.*: ","")
+		if removeCompany then
+			pluginName = string.gsub(pluginName, "%(.*%)","")
+		end
 		presetName = ""
 		success, presetName = reaper.TrackFX_GetPreset( track, 0, presetName)
 		fullName = string.format("%s%s%s",pluginName,separator, presetName)
@@ -19,6 +23,9 @@ for i=0, reaper.CountSelectedMediaItems(0)-1 do
 		presetName = ""
 		success, presetName = reaper.TrackFX_GetPreset( track, 0, presetName)
 		fullName = presetName
+	end
+	if removeSpaces then
+		fullName= string.gsub(fullName, "%s", "")
 	end
 	take = reaper.GetMediaItemTake(item, 0)
 	reaper.GetSetMediaItemTakeInfo_String(take, 'P_NAME', fullName, true)
--- SetTrackNameFromPluginPreset.lua
+++ SetTrackNameFromPluginPreset.lua
@@ -1,6 +1,8 @@
 -- SETTINGS
 separator = "-"
 includePluginName = true
+removeSpaces = true
+removeCompany = true
 
 -- MAIN
 trackID = 0
@@ -9,8 +11,10 @@ fullName = ""
 if includePluginName then
 	pluginName = ""
 	success, pluginName = reaper.TrackFX_GetFXName(track, 0, pluginName)
-    pluginName = string.gsub(pluginName, "VST: ","")
-    pluginName = string.gsub(pluginName, "VST3: ","")
+	pluginName = string.gsub(pluginName, "VST.*: ","")
+	if removeCompany then
+		pluginName = string.gsub(pluginName, "%(.*%)","")
+	end
 	presetName = ""
 	success, presetName = reaper.TrackFX_GetPreset( track, 0, presetName)
 	fullName = string.format("%s%s%s",pluginName,separator, presetName)
@@ -18,5 +22,8 @@ else
 	presetName = ""
 	success, presetName = reaper.TrackFX_GetPreset( track, 0, presetName)
 	fullName = presetName
+end
+if removeSpaces then
+	fullName= string.gsub(fullName, "%s", "")
 end
 reaper.GetSetMediaTrackInfo_String(track, "P_NAME", fullName, true)
